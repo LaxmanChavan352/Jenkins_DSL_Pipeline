f8bc2a50cffcca196862f6f4909825b068ae2a06
Laxman <chavanlaxman2015@gmail.com>
2021-05-26 10:28:59 +0530

    initial commit

---
+++ jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+
+    agent any
+
+    steges {
+
+                stage('Init') {
+
+                    steps {
+
+                        echo 'Hii inital state start'
+                    }
+                }
+                
+                stage('build') {
+
+                    steps {
+
+                        echo 'build stage start'
+                    }
+                }
+
+                stage('deploy') {
+
+                    steps {
+                        echo 'deployment stared '
+                    }
+                }
+    }
+}
